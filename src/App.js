55b67e6a45c3482ca8c1236f2cab2d2883f54b16
Dylan <dylanabozarth@gmail.com>
2020-10-04 22:13:18 -0700

    Useeffect

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [Score, setScore] = useState(0);
+  useEffect(() => {
+    document.title = `Your score is ${Score}`;
+  });
   return (
     <div className="App">
       <header className="App-header">
-        Space invaders 
-
+        
+{Score}
+<button onClick={() => setScore(Score + 1)}>Add Score</button>
       </header>
     </div>
   );
 }
 
 export default App;
+
+
