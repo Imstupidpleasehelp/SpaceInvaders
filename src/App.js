dffcd17a19ba2bead7aab6f7ea12723d44f72b50
Dylan <dylanabozarth@gmail.com>
2020-10-05 15:32:27 -0700

    controls

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
+import useKeyPress from "./hooks/useKeyPress";
 function App() {
-  const [Score, setScore] = useState(0);
+  const [score, setScore] = useState(0);
   useEffect(() => {
-    document.title = `Your score is ${Score}`;
+    document.title = `Your score is ${score}`;
   });
+const Moveleft = useKeyPress('a')
+const MoveUp = useKeyPress('w')
+const MoveDown = useKeyPress('s')
+const MoveRight = useKeyPress('d')
   return (
     <div className="App">
       <header className="App-header">
-        
-{Score}
-<button onClick={() => setScore(Score + 1)}>Add Score</button>
+        <div className="score">Your Score: {score}</div>
+        <div className="tank"></div>
+        {Moveleft && 'Left'}
+        {MoveUp && 'Up'}
+        {MoveDown && 'Down'}
+        {MoveRight && 'Right'}
+
       </header>
     </div>
   );
 }
 
 export default App;
-
-
